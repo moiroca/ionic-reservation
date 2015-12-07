99f45de8799689f9e0f536583cce11097c93dd8a
temoty.roca <temoty.roca@goabroad.com>
2015-12-07 19:28:57 +0800

    Change Password Mark UP

--- www/js/Controller/UserController.js
+++ www/js/Controller/UserController.js
@@ -4,5 +4,18 @@
 
 Reservation
 	.controller('UserController', ['$scope', function($scope) {
-		
+		$scope.password = { };
+		$scope.user = { };
+
+		/**
+		 * Change Password
+		 */
+		$scope.changePasswordSubmit = function(formValid) {
+
+			if (formValid) {
+				
+			} else {
+				
+			}
+		};
 	}]);
